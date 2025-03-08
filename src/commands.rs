d544f5ba4663795bc8fe72a1aafaf4fc958e4060
anomitroid <anomitroid2005@gmail.com>
2025-03-09 00:56:53 +0530

    moved run function into commands.rs

--- src/commands.rs
+++ src/commands.rs
@@ -1,7 +1,16 @@
-use crate::args;
+use crate::args::{EncodeArgs, DecodeArgs, RemoveArgs, PrintArgs, Cli, PngSpyArgs};
 use crate::Result;
 
-pub fn handle_encode(args: args::EncodeArgs) -> Result<()> {
+pub fn run(args: Cli) -> Result<()> {
+    match args.command {
+        PngSpyArgs::Encode(args) => handle_encode(args),
+        PngSpyArgs::Decode(args) => handle_decode(args),
+        PngSpyArgs::Remove(args) => handle_remove(args),
+        PngSpyArgs::Print(args) => handle_print(args),
+    }
+}
+
+pub fn handle_encode(args: EncodeArgs) -> Result<()> {
     println!("Encode command invoked:");
     println!("  File path: {:?}", args.file_path);
     println!("  Chunk type: {}", args.chunk_type);
@@ -11,7 +20,7 @@ pub fn handle_encode(args: args::EncodeArgs) -> Result<()> {
     Ok(())
 }
 
-pub fn handle_decode(args: args::DecodeArgs) -> Result<()> {
+pub fn handle_decode(args: DecodeArgs) -> Result<()> {
     println!("Decode command invoked:");
     println!("  File path: {:?}", args.file_path);
     println!("  Chunk type: {}", args.chunk_type);
@@ -19,7 +28,7 @@ pub fn handle_decode(args: args::DecodeArgs) -> Result<()> {
     Ok(())
 }
 
-pub fn handle_remove(args: args::RemoveArgs) -> Result<()> {
+pub fn handle_remove(args: RemoveArgs) -> Result<()> {
     println!("Remove command invoked:");
     println!("  File path: {:?}", args.file_path);
     println!("  Chunk type: {}", args.chunk_type);
@@ -27,7 +36,7 @@ pub fn handle_remove(args: args::RemoveArgs) -> Result<()> {
     Ok(())
 }
 
-pub fn handle_print(args: args::PrintArgs) -> Result<()> {
+pub fn handle_print(args: PrintArgs) -> Result<()> {
     println!("Print command invoked:");
     println!("  File path: {:?}", args.file_path);
     // TODO: Add print logic.
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,13 @@ mod args;
 mod commands;
 
 use clap::Parser;
-use args::{Cli, PngSpyArgs};
+use args::Cli;
+use commands::run;
 
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
-
-    match cli.command {
-        PngSpyArgs::Encode(args) => commands::handle_encode(args),
-        PngSpyArgs::Decode(args) => commands::handle_decode(args),
-        PngSpyArgs::Remove(args) => commands::handle_remove(args),
-        PngSpyArgs::Print(args) => commands::handle_print(args),
-    }
+    run(cli)
 }
