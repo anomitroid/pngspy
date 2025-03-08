a6383903b11fc0c5243b482b964d3ad867ac8f72
anomitroid <anomitroid2005@gmail.com>
2025-03-09 00:16:20 +0530

    made basic main.rs with crate-level error aliases

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+
+fn main() -> Result<()> {
+    todo!()
 }
